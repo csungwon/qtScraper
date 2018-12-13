cdca8265bdd8970bc71e9a88500b8f5ddba4774d
Sungwon Cho <csungwon@berkeley.edu>
2018-12-12 21:12:48 -0800

    add hello world apollo server express

---
+++ packages/server/src/index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const app = express();
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String!
+    author: String!
+  }
+
+  type Query {
+    books: [Book!]!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.applyMiddleware({
+  app,
+  path: '/graphql'
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Apollo Server running on port ${PORT}!`);
+});
